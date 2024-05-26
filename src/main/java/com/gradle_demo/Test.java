c0050667262af51e28d290fad9a8daeffe1c28c1
Kiran <kiranshetty3095@gmail.com>
2024-05-26 19:21:34 +0530

    New Test Program

---
+++ src/main/java/com/gradle_demo/Test.java
@@ -0,0 +1,15 @@
+package com.gradle_demo;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        int radius;
+        double pi = 3.142, area;
+        radius = 3;
+        // calculating the area of the circle
+        area = pi * radius * radius;
+        // printing the area of the circle
+        System.out.println("Area of circle is :" + area);
+    }
+}
